8d98f0255267ab9e58d7f940861e8b8c155db395
SurajKarki <suraj.karki500@protonmail.com>
2022-12-21 19:05:58 +0545

    Make a drawer for creating subtask

--- react-admin-dashboard-refine-demo/src/components/subtask/create.tsx
+++ react-admin-dashboard-refine-demo/src/components/subtask/create.tsx
@@ -7,71 +7,102 @@ import {
   Button,
   Icons,
   Typography,
+  DrawerProps,
+  Drawer,
+  Grid,
 } from "@pankod/refine-antd";
 import { BaseKey, useCreate } from "@pankod/refine-core";
 
 type CreateSubtaskProps = {
+  drawerProps: DrawerProps;
   formProps: FormProps;
   saveButtonProps: ButtonProps;
   taskId?: BaseKey;
+  closeCreate: () => void;
 };
 
 export const CreateSubtask: React.FC<CreateSubtaskProps> = ({
   formProps,
   taskId,
+  drawerProps,
+  closeCreate,
 }) => {
   const { mutate } = useCreate();
   const { form } = formProps;
+  const breakpoint = Grid.useBreakpoint();
 
+  const handleSubmit = async (e: any, isRedirect: boolean) => {
+    e.preventDefault();
+    mutate(
+      {
+        resource: "subtasks",
+        values: {
+          title: form?.getFieldValue("title"),
+          todo: taskId,
+        },
+      },
+      {
+        onError: (error, variables, context) => {},
+        onSuccess: (data, variables, context) => {
+          form?.resetFields();
+          if (isRedirect) {
+            closeCreate();
+          }
+        },
+      }
+    );
+  };
   return (
-    <Create
-      resource="subtasks"
-      breadcrumb={null}
-      headerProps={{
-        title: <Typography.Text>Add Subtask</Typography.Text>,
-      }}
-      footerButtons={() => (
-        <>
-          <Button
-            onClick={(e) => {
-              e.preventDefault();
-              mutate(
-                {
-                  resource: "subtasks",
-                  values: {
-                    title: form?.getFieldValue("title"),
-                    todo: taskId,
-                  },
-                },
-                {
-                  onError: (error, variables, context) => {},
-                  onSuccess: (data, variables, context) => {
-                    form?.resetFields();
-                  },
-                }
-              );
-            }}
-            type="primary"
-            icon={<Icons.SaveFilled />}
-          >
-            Save
-          </Button>
-        </>
-      )}
+    <Drawer
+      {...drawerProps}
+      width={breakpoint.sm ? "500px" : "80%"}
+      bodyStyle={{ padding: 0 }}
+      zIndex={1001}
     >
-      <Form form={form} layout="vertical">
-        <Form.Item
-          label="Title"
-          name="title"
-          rules={[
-            {
-              required: true,
-            },
-          ]}
-        >
-          <Input prefix={<Icons.FileTextOutlined />} allowClear size="large" />
-        </Form.Item>
-      </Form>
-    </Create>
+      <Create
+        resource="subtasks"
+        breadcrumb={null}
+        goBack={null}
+        headerProps={{
+          title: <Typography.Text>Add Subtask</Typography.Text>,
+        }}
+        footerButtons={() => (
+          <>
+            <Button
+              onClick={(e) => handleSubmit(e, true)}
+              type="primary"
+              icon={<Icons.SaveFilled />}
+            >
+              Save
+            </Button>
+            <Button
+              onClick={(e) => handleSubmit(e, false)}
+              type="primary"
+              icon={<Icons.SaveFilled />}
+            >
+              Save and add another
+            </Button>
+          </>
+        )}
+      >
+        <Form form={form} layout="vertical">
+          <Form.Item
+            label="Title"
+            name="title"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input
+              prefix={<Icons.FileTextOutlined />}
+              allowClear
+              size="large"
+            />
+          </Form.Item>
+        </Form>
+      </Create>
+    </Drawer>
   );
 };
